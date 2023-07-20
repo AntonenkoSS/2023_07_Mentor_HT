204f59a8ec969eb9e69f9b60f438672ea37f9d57
AntonenkoSS <ssantonenko@gmail.com>
2023-07-20 20:52:29 +0300

    +Floor+Carpet+Calculator

---
+++ src/main/java/Floor.java
@@ -0,0 +1,25 @@
+public class Floor {
+    double width;
+    double length;
+
+    public Floor(double width, double length) {
+        if (width<0){width=0;}
+        if (length<0){length=0;}
+
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea(){
+        double area = getWidth()*getLength();
+        return area;
+    }
+}
